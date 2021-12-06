8451b18d6fb6b71da409f53c60511b1279547c59
Vladislav Alexeyev <vladislav.j.alexeyev@gmail.com>
2021-12-06 11:10:20 +0100

    Init

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# install docker, nginx, make, curl and other needed tools:
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+	make \
+    gnupg \
+    nginx \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# install docker-compose: 
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+# firewall configuration:
+
+ufw allow 80,443,3000,996,7946,4789,2377/tcp; ufw allow 7946,4789,2377/udp;
+
+	
