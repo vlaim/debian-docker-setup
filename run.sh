d1f09f470b6de47654f1ba7d0483283f1b44a78a
Vladislav Alexeyev <vladislav.j.alexeyev@gmail.com>
2021-12-06 11:16:03 +0100

    Skip prompt

--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@
 # install docker, nginx, make, curl and other needed tools:
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -29,5 +30,3 @@ sudo chmod +x /usr/local/bin/docker-compose
 # firewall configuration:
 
 ufw allow 80,443,3000,996,7946,4789,2377/tcp; ufw allow 7946,4789,2377/udp;
-
-	
