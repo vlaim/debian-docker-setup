097254d9d86400be244a9415753ef29db8a60237
Vladislav Alexeyev <vladislav.j.alexeyev@gmail.com>
2021-12-06 11:12:12 +0100

    Fix order for update

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # install docker, nginx, make, curl and other needed tools:
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
@@ -13,11 +19,6 @@ sudo apt-get install \
     docker-ce-cli \
     containerd.io
     lsb-release
-
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install docker-compose: 
 
